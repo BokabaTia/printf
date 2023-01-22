32fd0a51fae6417099dee70e6cc9aaeaf594c71d
Chukwuemeka <78231106+Kachu1212@users.noreply.github.com>
2023-01-22 19:54:01 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@
  * Return: the length of the string.
  */
 
-int _printf(const char *const format, ...)
+int _printf(const char *format, ...)
 {
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
