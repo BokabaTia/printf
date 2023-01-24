999a74187e1bf951db1bad5c4d7763f5972392e6
Nwosu Chukwuemeka K <78231106+Kachu1212@users.noreply.github.com>
2023-01-24 11:21:03 +0100

    get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -17,7 +17,7 @@ int get_precision(const char *format, int *i, va_list list)
 	if (format[curr_i] != '.')
 		return (precision);
 
-	precision = 0
+	precision = 0;
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
