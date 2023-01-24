18a6f88e10d3ce58533163b7f3b73a8cb9b31f1f
BokabaTia <117736323+BokabaTia@users.noreply.github.com>
2023-01-24 18:22:46 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -18,7 +18,7 @@ int is_printable(char c)
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
+ * @ascii_code: ASSCI CODE
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
