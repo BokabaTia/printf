d39ece46a194dea2c666458c97ef8a7ba3a90745
BokabaTia <117736323+BokabaTia@users.noreply.github.com>
2023-01-24 18:23:11 +0200

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -10,7 +10,7 @@
  * @precision: precision specifier
  * @size: Size specifier
  *
- * Return: Number of chars printed.
+ * Return: Number of chars printed
  */
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
