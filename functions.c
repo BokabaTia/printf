deeeafc532d07a5ca96bbba9dd65ec9fedf82e4e
BokabaTia <117736323+BokabaTia@users.noreply.github.com>
2023-01-24 18:14:07 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -150,7 +150,7 @@ int print_int(va_list types, char buffer[],
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Numbers of char printed.
+ * Return: Numbers of char printed
  */
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
