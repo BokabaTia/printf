284ad2b756e6d820a988174cf1c5d3fe5527e04a
BokabaTia <117736323+BokabaTia@users.noreply.github.com>
2023-01-24 18:36:43 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -4,7 +4,7 @@
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
  *
- * Return: 1 if c is printable, 0 otherwise
+ * Return: 1 if c is printable, 0 otherwise.
  */
 int is_printable(char c)
 {
